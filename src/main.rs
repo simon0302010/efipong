4fc22643e44d47ad22715823d6880105e7e49fc8
simon0302010 <simon0302010@gmail.com>
2025-12-16 21:02:07 +0100

    allow user to exit by pressing q

--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,10 @@ mod buffer;
 
 use core::time::Duration;
 
-use uefi::prelude::*;
 use uefi::proto::console::gop::{BltPixel, GraphicsOutput};
+use uefi::proto::console::text::Key;
 use uefi::{boot, Result};
+use uefi::{prelude::*, Char16};
 
 use crate::buffer::Buffer;
 
@@ -29,11 +30,35 @@ fn game() -> Result {
     let rec_w = 50;
     let rec_h = 30;
 
-    buffer.clear();
-    buffer.rectangle((width / 2) - (rec_w / 2), (height / 2) - (rec_h / 2), rec_w, rec_h, BltPixel::new(255, 0, 0), false);
-    let _ = buffer.blit(&mut gop);
+    let mut running = true;
 
-    boot::stall(Duration::from_secs(10));
+    while running {
+        while let Ok(Some(key)) = system::with_stdin(|stdin| stdin.read_key()) {
+            match key {
+                Key::Printable(c) => {
+                    if c == Char16::try_from('q').unwrap_or_default()
+                        || c == Char16::try_from('Q').unwrap_or_default()
+                    {
+                        running = false;
+                    }
+                }
+                _ => {}
+            }
+        }
+
+        buffer.clear();
+        buffer.rectangle(
+            (width / 2) - (rec_w / 2),
+            (height / 2) - (rec_h / 2),
+            rec_w,
+            rec_h,
+            BltPixel::new(255, 0, 0),
+            false,
+        );
+        let _ = buffer.blit(&mut gop);
+
+        boot::stall(Duration::from_millis(16));
+    }
 
     Ok(())
 }
